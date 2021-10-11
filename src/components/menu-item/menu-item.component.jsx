4543ca2a48fa609ca8346b045051d70f34880cdd
Víctor Moreno Maya <victor.moreno.maya@gmail.com>
2021-10-11 16:46:33 +0200

    Added images to menu-items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,11 @@
 import './menu-item.styles.scss'
 
-const MenuItem =({ title }) =>(
-    <div className='menu-item'>
+const MenuItem =({ title, imageUrl, size}) =>(
+    <div 
+    style ={{
+        backgroundImage:`url(${imageUrl})`
+    }}
+    className={`${size} menu-item`}>
         <div className='conente'>
             <h1 className='title'>{title}</h1>
             <span className='subtitle'>SHOP NOW</span>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,16 +1,10 @@
 import './homepage.style.scss';
 
-import MenuItem from '../../components/menu-item/menu-item.component';
+import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => (
     <div className='homepage'>
-        <div className= 'directory-menu'>
-            <MenuItem className='menu-item' title='HATS' />
-            <MenuItem className='menu-item' title='JACKETS' />
-            <MenuItem className='menu-item' title='SHOES' />
-            <MenuItem className='menu-item' title='MEN' />
-            <MenuItem className='menu-item' title='WOMEN' />     
-        </div>    
+        <Directory/>  
     </div>
 )
 
