dc4dfcfe058f5357280afa31a1cff1d041cf867b
Víctor Moreno Maya <victor.moreno.maya@gmail.com>
2021-10-11 19:39:42 +0200

    Added hats route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router';
 import './App.css';
+
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
 function App() {
   return (
-    <div className="App">
-      <HomePage></HomePage>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
