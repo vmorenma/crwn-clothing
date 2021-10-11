62c085c150811fe953fb3996dfe58605067a359f
Víctor Moreno Maya <victor.moreno.maya@gmail.com>
2021-10-11 14:50:00 +0200

    First commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import './homepage.style.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className= 'directory-menu'>
+            <div className='menu-item'>
+                <div className='conente'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='conente'>
+                    <h1 className='title'>JACKETS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='conente'>
+                    <h1 className='title'>SHOWES</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='conente'>
+                    <h1 className='title'>MEN</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='conente'>
+                    <h1 className='title'>WOMEN</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>    
+    </div>
+)
+
+export default HomePage;
