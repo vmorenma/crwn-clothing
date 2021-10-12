c0f11be19765170ee783d330ce30510f2279e0ed
Víctor Moreno Maya <victor.moreno.maya@gmail.com>
2021-10-12 12:08:53 +0200

    Added header component, logo  and links to home and shop

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import './App.css';
 
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/hats' component={HatsPage}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
