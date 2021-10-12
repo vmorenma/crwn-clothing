eb02a23674b3c2bdfa603d1747f895af7d07755d
Víctor Moreno Maya <victor.moreno.maya@gmail.com>
2021-10-12 13:28:10 +0200

    Added sign in form and custombutton

---
+++ src/pages/sign/sign.component.jsx
@@ -0,0 +1,11 @@
+import "./sign.styles.scss";
+
+import SignIn from "../../components/sign-in/sign-in.controller";
+
+const SignPage = () => (
+  <div className="sign">
+    <SignIn />
+  </div>
+);
+
+export default SignPage;
